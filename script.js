51be96e09e2985617f43411a785adab7b02438d9
diogodaniel975-sketch <diogodaniel975@gmail.com>
2026-04-05 12:35:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,48 +4,61 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-class Heart {
-  constructor() {
-    this.x = Math.random() * canvas.width;
-    this.y = canvas.height + 20;
-    this.size = Math.random() * 25 + 10;
-    this.speed = Math.random() * 2 + 1;
-    this.color = `rgba(${Math.floor(Math.random()*255)}, 
-                       ${Math.floor(Math.random()*100)}, 
-                       ${Math.floor(Math.random()*150)}, 0.8)`;
-  }
-  draw() {
-    ctx.beginPath();
-    ctx.moveTo(this.x, this.y);
-    ctx.bezierCurveTo(this.x - this.size, this.y - this.size,
-                      this.x - this.size, this.y + this.size / 2,
-                      this.x, this.y + this.size);
-    ctx.bezierCurveTo(this.x + this.size, this.y + this.size / 2,
-                      this.x + this.size, this.y - this.size,
-                      this.x, this.y);
-    ctx.fillStyle = this.color;
-    ctx.fill();
-  }
-  update() {
-    this.y -= this.speed;
-    this.draw();
-  }
+let hearts = [];
+
+function randomColor() {
+  const colors = ["#ff4d6d", "#ff99ac", "#c77dff", "#80ed99", "#ffd166", "#5dade2"];
+  return colors[Math.floor(Math.random() * colors.length)];
 }
 
-let hearts = [];
+function createHeart() {
+  hearts.push({
+    x: Math.random() * canvas.width,
+    y: canvas.height + 20,
+    size: Math.random() * 20 + 10,
+    speed: Math.random() * 1 + 0.5,
+    color: randomColor(),
+    opacity: Math.random()
+  });
+}
+
+function drawHeart(x, y, size, color, opacity) {
+  ctx.globalAlpha = opacity;
+  ctx.fillStyle = color;
+
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+
+  ctx.bezierCurveTo(x - size, y - size, x - size * 2, y + size / 2, x, y + size);
+  ctx.bezierCurveTo(x + size * 2, y + size / 2, x + size, y - size, x, y);
+
+  ctx.fill();
+  ctx.globalAlpha = 1;
+}
 
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  if (hearts.length < 150) {
-    hearts.push(new Heart());
+
+  if (hearts.length < 100) {
+    createHeart();
   }
+
   hearts.forEach((heart, index) => {
-    heart.update();
-    if (heart.y < -30) {
+    heart.y -= heart.speed;
+
+    drawHeart(heart.x, heart.y, heart.size, heart.color, heart.opacity);
+
+    if (heart.y < -20) {
       hearts.splice(index, 1);
     }
   });
+
   requestAnimationFrame(animate);
 }
 
 animate();
+
+window.addEventListener("resize", () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
